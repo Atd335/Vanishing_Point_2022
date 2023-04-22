f0b72e4bedccc0fff25b1a09585f3996cfa40e97
Max <ms12047@nyu.edu>
2023-04-22 11:40:40 -0400

    2A first floor population done

--- VP Core System Redesign/Assets/Core Systems/Scripts/3D Space Scripts/Constant Variables/ColorInfo.cs
+++ VP Core System Redesign/Assets/Core Systems/Scripts/3D Space Scripts/Constant Variables/ColorInfo.cs
@@ -6,7 +6,7 @@ public class ColorInfo : MonoBehaviour
 {
     public static Color platformColor_3D = new Color(.6f,1,1,1);
     public static Color cutoutColor_3D = new Color(1,1,1,1);
-    public static Color damageColor_3D = new Color(1,0,0,1);
+    public static Color damageColor_3D = new Color(1,0.7f,0.7f,1);
 
     public static Color platformColor_2D = new Color(0,0,0,1);
     public static Color cutoutColor_2D = new Color(1,1,1,1);
